27eb42059681e7ef94c69ae102f6f6420112f780
Beatriz Torres <btorre0@csu.fullerton.edu>
2024-09-23 14:09:37 -0700

    format

--- fib.c
+++ fib.c
@@ -3,50 +3,43 @@
 // 09.06.24
 // btorre0@csu.fullerton.edu
 // Github Username: Btorre0
+
 #include <stdio.h>
 #include <stdint.h>
 
-
 // Recursive function
-unsigned long long int fibRec( int num ) {
-
-   if (num <= 1) {
-
+unsigned long long int fibRec(int num)
+{
+   if (num <= 1)
+   {
       return 0;
-
-   } else if (num == 2) {
-
+   }
+   else if (num == 2)
+   {
       return 1;
    }
 
    return fibRec(num - 1) + fibRec(num - 2);
 }
 
-
-
 // Iterative function
-unsigned long long int fibIter( int num ) {
-
+unsigned long long int fibIter(int num)
+{
    unsigned long long int a = 0;
    unsigned long long int b = 1;
    unsigned long long int c;
 
-   for (int i = 3; i <= num; i++) {
-
-      c = a + b;
-      a = b;
-      b = c;
-   }
+   c = a + b;
+   a = b;
+   b = c;
 
    return b;
 }
 
-
-
-int main( int argc , char *argv[] ) {
-
-   if (argc < 3) {
-
+int main(int argc, char *argv[])
+{
+   if (argc < 3)
+   {
       printf("Usage: %s <integer> <r/i> <filename>\n", argv[0]);
 
       return 1;
@@ -58,11 +51,12 @@ int main( int argc , char *argv[] ) {
 
    sscanf(argv[1], "%d", &num);
 
-   if (argc == 4) {
-
+   if (argc == 4)
+   {
       FILE *file = fopen(argv[3], "r");
 
-      if (file == NULL) {
+      if (file == NULL)
+      {
 
          printf("file doesnt exist\n");
 
@@ -73,14 +67,16 @@ int main( int argc , char *argv[] ) {
 
       fclose(file);
    }
-   
 
    num += file_num;
 
-   if (argv[2][0] == 'r') {
-      
+   if (argv[2][0] == 'r')
+   {
+
       compute = fibRec(num);
-   } else {
+   }
+   else
+   {
 
       compute = fibIter(num);
    }
